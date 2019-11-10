1fa7e86fa31d887c358ab79001e007cf2c3efb26
Zach Johnson <ZachjDev2121@gmail.com>
2019-11-10 07:28:55 -0500

    test remote origin

---
+++ app.js
@@ -0,0 +1,17 @@
+const express  = require("express"),
+      mongoose = require("mongoose"),
+      app      = express(),
+      port     = 3000;
+
+
+//Mongoose setup:
+mongoose.connect("mongodb://localhost", {useNewUrlParser: true});
+
+
+// routes, for now
+app.get("/", (req, res) => res.send("Hi"));
+
+//Hello, GitHub
+
+
+app.listen(port, () => console.log("server up"))
