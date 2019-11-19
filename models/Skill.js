b849cee863c16aeceb392961eee6118d22ee15f2
Know Nothing <Knownothingclubbers.gmail.com>
2019-11-19 06:51:52 -0500

    make skill names unique

--- models/Skill.js
+++ models/Skill.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const skillSchema = new mongoose.Schema({
-    name: String,
+    name: {type: String, unique: true},
     description: String,
     competency: {type: mongoose.Schema.Types.ObjectId, ref: 'Competency'},
     number: Number,
