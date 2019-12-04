5060c49109a4ee3e49c2014a8f67d48782936721
Know Nothing <Knownothingclubbers.gmail.com>
2019-12-03 19:42:32 -0500

    update schema for delete routes

--- models/Competency.js
+++ models/Competency.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 
 const competencySchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    number: Number,
-    topic: [{name: String, description: String}],
-    skills: [{
-        name: String,
-        description: String,
-        number: Number,
-        subSkills: [{    
-            name: String,
-            description: String,
-            number: Number}]
-    }]
+  name: String,
+  description: String,
+  number: Number,
+  topic: [{ name: String, description: String }],
+  skillsArray: [Number],
+  skills: [
+    {
+      name: String,
+      number: Number,
+      subSkillArray: [Number],
+      subSkills: [
+        {
+          name: String,
+          number: Number
+        }
+      ]
+    }
+  ]
 });
 
 const Competency = mongoose.model("Competency", competencySchema);
