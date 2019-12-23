bc74f5bd6453812f4d9849b1a7a690e41693fc83
Know Nothing <Knownothingclubbers.gmail.com>
2019-12-23 05:59:13 -0500

    added create role route

--- models/Role.js
+++ models/Role.js
@@ -1,9 +1,20 @@
 const mongoose = require("mongoose");
 
 const roleSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    skills: [{type: mongoose.Schema.Types.ObjectId, ref: "skill"}]
-})
+  name: String,
+  description: String,
+  competenciesAndSkills: [
+    {
+      competency: { type: mongoose.Schema.Types.ObjectId, ref: "Competency"},
+      skills: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Skill"
+        }
+      ]
+    }
+  ]
+});
+roleSchema.plugin(require("mongoose-autopopulate"));
 const Role = mongoose.model("Role", roleSchema);
-module.exports = Role
+module.exports = Role;
--- routes/Roles.js
+++ routes/Roles.js
@@ -4,19 +4,22 @@ const express = require("express"),
   Competency = require("../models/Competency.js");
 
 /* This whole function could use some TLC*/
+/* This function creates an object, where each key is a competency and each value is an array of specific skills. if 
+a competency is added by itself, then the array will be a 0, and that will eventually tell the application to add all
+skills from that competency.*/
 function createSkillsMap(skills) {
   skillsMap = {};
   skills = skills.split(",");
   skills.forEach(skill => {
-    if (!/[1-9].[1-9]/g.test(skill)) {
-      throw Error("ahhh");
+    if (!/[1-9]\.[1-9]/g.test(skill) && !/[1-9]$/g.test(skill)) {
+      throw Error(`Regular Expression Test Failed for ${skill}`);
     }
     //I really feel like the next 4 lines could be combined into one.
     if (!skillsMap[skill[0]]) {
       skillsMap[skill[0]] = [];
     }
-    // It probably would be fine to just always psuh skill[2] and accept the undefined
-    // but for now, I like the 0 instead. Its reassuring to work with a known value.
+    // It probably would work to just always push skill[2] and accept the undefined
+    // but for now, I like the 0 instead. It's reassuring to work with a known value.
     skill[2]
       ? skillsMap[skill[0]].push(Number(skill[2]))
       : skillsMap[skill[0]].push(0);
@@ -26,40 +29,59 @@ function createSkillsMap(skills) {
 //roles routes
 //index
 router.get("/", (req, res) => {
-  Role.find({}, (err, allRoles) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("roles/index", { roles: allRoles });
-    }
-  });
+  Role.find({})
+    .populate({ path: "competenciesAndSkills.competency" })
+    .populate({ path: "competenciesAndSkills.skills" })
+    .exec((err, allRoles) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("roles/index", { roles: allRoles });
+      }
+    });
 });
 //New
 router.get("/new", (req, res) => {
-  res.render("roles/new", { name: false, description: false });
+  res.render("roles/new", { name: false, description: false }); //as to not mess up the ejs when opening the form for the first time. This would probably be better handled with cookies, but I don't know how to use those yet.
 });
 //Create
 router.post("/", (req, res) => {
-  let name = req.body.name;
-  let description = req.body.description;
+  let roleName = req.body.name;
+  let roleDescription = req.body.description;
   try {
-    let skills = createSkillsMap(req.body.skills);
-    console.log(skills);
-    compet = Competency.findOne({number: 1}) //returns an array
-    compet.then( (compett) => {
-      addskills = compett.skills.filter(skill => skill.number == 2)
-      Role.create({
-        name: name,
-        description: description, 
-      }).then((role) => {
-        role.skills.push(addskills[0]);
-        role.save()
-        console.log(role)
+    Role.create({ name: roleName, description: roleDescription })
+      .then(role => {
+        let skillsObj = createSkillsMap(req.body.skills); // right now there's no protection against returning an onject key with 0 and other numbers. should be one or the other.
+        let skillKeys = Object.keys(skillsObj);
+        let newSkillsArray = [];
+        skillKeys.forEach(key => {
+          keyAsNum = Number(key);
+          Competency.findOne({ number: keyAsNum })
+            .populate("skills")
+            .then(competency => {
+              skillsObj[key].forEach(el => {
+                if (el == 0) {
+                  newSkillsArray.push(...competency.skills);
+                } else {
+                  index = competency.skills.findIndex(
+                    element => element.number == el
+                  );
+                  newSkillsArray.push(competency.skills[index]);
+                }
+              });
+              console.log(competency._id);
+              console.log(newSkillsArray);
+              role.competenciesAndSkills.push({
+                competency: competency._id,
+                skills: [...newSkillsArray]
+              });
+              role.save();
+            });
+        });
       })
-    }); 
-    res.redirect("/roles");
-
+      .then(setTimeout(() => res.redirect("/roles"), 0));
   } catch (error) {
+    console.log("akljsdhfalskjhflaskdjhfdlkasjhdflkjh");
     console.log(error);
     res.render("roles/new", { name: name, description: description });
   }
@@ -67,13 +89,16 @@ router.post("/", (req, res) => {
 
 //Show
 router.get("/:id", (req, res) => {
-  Role.findById(req.params.id).populate("skills").exec((err, role) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("roles/show", { role: role });
-    }
-  });
+  Role.findById(req.params.id)
+  .populate({ path: "competenciesAndSkills.competency" })
+  .populate({ path: "competenciesAndSkills.skills" })
+    .exec((err, role) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("roles/show", { role: role });
+      }
+    });
 });
 //Edit
 router.get("/:id/edit", (req, res) => res.send("This is the Roles EDIT route"));
