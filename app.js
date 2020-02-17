88695d5c48fef2d9a28aea25b5354248594a2c60
Zach Johnson <Knownothingclubbers.gmail.com>
2020-02-17 18:38:10 -0500

    environmental variable

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const competencyRoutes = require("./routes/Competencies"),
 //Mongoose setup:
 //mongodb://localhost/CompetencyTracker
 //I've been having troubling keeing environmental variables straight between my computers, so until this is hosted anywhere, mongoose will stay connected like this.
-mongoose.connect("mongodb://localhost/CompetencyTracker", {
+mongoose.connect(ENV.DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
