87403efe96608e719e440e505e72a847944379f3
Zach Johnson <Zachjdev2121@gmail.com>
2020-08-05 20:54:46 -0400

    basic dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,20 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 
 const Competency = require('../models/Competency');
-const DeletedCompetencyCounter = require('../models/DeletedCompetencyCounter');
 const Role = require('../models/Role');
 
 
 router.get('/', (req, res) => {
   if (req.session.isLoggedIn) {
     const promises = [];
-    promises.push(Competency.find({ institution: req.user.institutionName }, null, { sort: { dateUpdated: 1 }, limit: 5 }));
-    promises.push(Role.find({ institution: req.user.institutionName }, null, { sort: { dateUpdated: 1 }, limit: 5 }));
+    promises.push(Competency.find(
+      { institution: req.user.institutionName },
+      null, { sort: { dateUpdated: -1 }, limit: 5 },
+    ));
+    promises.push(Role.find(
+      { institution: req.user.institutionName },
+      null, { sort: { dateUpdated: -1 }, limit: 5 },
+    ));
     Promise.all(promises).then((values) => {
       const roles = values[1];
       const competencies = values[0];
